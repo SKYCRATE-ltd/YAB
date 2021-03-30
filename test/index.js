14a6b0c08676d7f3ab692d95d291aca909693c9d
dumpstergoon <graham_daniel_robertson@protonmail.com>
2021-03-30 22:05:00 +0100

    ya, boi

---
+++ test/index.js
@@ -0,0 +1,5 @@
+import def, { beans, bread as toast } from "./module.js";
+import * as module from "./module.js";
+
+// We won't export anything here...
+// Just make sure it converts properly... maybe
